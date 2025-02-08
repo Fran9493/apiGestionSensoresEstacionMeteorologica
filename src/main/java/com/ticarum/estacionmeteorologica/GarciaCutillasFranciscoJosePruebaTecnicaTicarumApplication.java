306d85b8c3b16469e6537aadf2a2b65b9596518d
Francisco José García Cutillas <fran.garcia93@gmail.com>
2025-02-08 13:36:22 +0100

    Primer commit
    Commit al inicio del proyecto

---
+++ src/main/java/com/ticarum/estacionmeteorologica/GarciaCutillasFranciscoJosePruebaTecnicaTicarumApplication.java
@@ -0,0 +1,13 @@
+package com.ticarum.estacionmeteorologica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarciaCutillasFranciscoJosePruebaTecnicaTicarumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GarciaCutillasFranciscoJosePruebaTecnicaTicarumApplication.class, args);
+	}
+
+}
